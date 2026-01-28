cd5c3da9b6fa143ab1a9fa6e266eb190f6446430
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-01-28 18:49:33 +0500

    Initiated the Product List Component for the Home Page Component

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,8 @@
+// <== PRODUCT LIST COMPONENT ==>
+const ProductList = () => {
+  // RETURNING PRODUCT LIST COMPONENT
+  return <div>ProductList</div>;
+};
+
+// <== EXPORT PRODUCT LIST COMPONENT ==>
+export default ProductList;
