4e468a6efea216f00f5e3df3c76d969ebaf73817
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-01-28 18:46:03 +0500

    Updated the Home Page File to Add the Banner Image Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-const Homepage = () => {
-  return <div className="">E-Commerce Platform</div>;
+// <== IMPORTS ==>
+import { JSX } from "react";
+import Image from "next/image";
+
+// <== HOMEPAGE COMPONENT ==>
+const Homepage = (): JSX.Element => {
+  // RETURNING HOMEPAGE COMPONENT
+  return (
+    <div className="">
+      {/* BANNER IMAGE */}
+      <div className="relative aspect-[3/1] mb-12">
+        <Image src={"/featured.png"} alt="Featured Product" fill />
+      </div>
+    </div>
+  );
 };
 
+// <== EXPORT HOMEPAGE COMPONENT ==>
 export default Homepage;
