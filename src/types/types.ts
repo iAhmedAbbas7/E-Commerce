c4ccb0d6304ea4a4cb04f5e8220acf7387cf3324
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-01-29 18:33:17 +0500

    Introduced Two New Types CartItemType & CartItemListType

--- src/types/types.ts
+++ src/types/types.ts
@@ -18,5 +18,34 @@ export type ProductType = {
   images: Record<string, string>;
 };
 
+// <== CART ITEM TYPE ==>
+export type CartItemType = {
+  // <== PRODUCT ID ==>
+  id: string | number;
+  // <== PRODUCT NAME ==>
+  name: string;
+  // <== PRODUCT DESCRIPTION ==>
+  description: string;
+  // <== PRODUCT SHORT DESCRIPTION ==>
+  shortDescription: string;
+  // <== PRODUCT PRICE ==>|
+  price: number;
+  // <== PRODUCT CATEGORY ==>
+  sizes: string[];
+  // <== PRODUCT TAGS ==>
+  colors: string[];
+  // <== PRODUCT IMAGES ==>
+  images: Record<string, string>;
+  // <== SELECTED SIZE ==>
+  selectedSize: string;
+  // <== SELECTED COLOR ==>
+  selectedColor: string;
+  // <== SELECTED QUANTITY ==>
+  quantity: number;
+};
+
 // <== PRODUCT LIST TYPE ==>
 export type ProductListType = ProductType[];
+
+// <== CART ITEM LIST TYPE ==>
+export type CartItemListType = CartItemType[];
