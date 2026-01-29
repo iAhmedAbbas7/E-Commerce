b639d3fa34ad9872e57c9f5adeff164499011554
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-01-29 18:35:11 +0500

    Updated the Product List Component with the Updated UI Section

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,47 @@
+// <== IMPORTS ==>
+import { JSX } from "react";
+import Link from "next/link";
+import Filter from "./Filter";
+import Categories from "./Categories";
+import ProductCard from "./ProductCard";
+import { ShoppingBag } from "lucide-react";
+import { PRODUCTS } from "@/utils/constants";
+
 // <== PRODUCT LIST COMPONENT ==>
-const ProductList = () => {
+const ProductList = ({
+  category,
+  params,
+}: {
+  category: string;
+  params: "homepage" | "products";
+}): JSX.Element => {
   // RETURNING PRODUCT LIST COMPONENT
-  return <div>ProductList</div>;
+  return (
+    // MAIN CONTAINER
+    <div className="w-full">
+      {/* CATEGORIES COMPONENT */}
+      <Categories />
+      {/* CONDITIONAL PRODUCTS FILTER */}
+      {params === "products" && <Filter />}
+      {/* PRODUCT CARDS CONTAINER */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-12">
+        {/* PRODUCT CARDS RENDERING */}
+        {PRODUCTS.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      {/* VIEW ALL PRODUCTS */}
+      <div className="flex justify-end mt-4">
+        <Link
+          className="rounded-md shadow-md px-2 py-1 bg-gray-100 hover:bg-gray-200 transition-all duration-300 text-sm font-semibold text-gray-600 flex items-center gap-2"
+          href={category ? `/products?category=${category}` : "/products"}
+        >
+          <ShoppingBag className="w-4 h-4" />
+          View All Products
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 // <== EXPORT PRODUCT LIST COMPONENT ==>
