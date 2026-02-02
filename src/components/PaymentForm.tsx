491ee50a0f9b4679114012bc78739c02bb0d6e87
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-02 15:59:48 +0500

    Add Payment Form Component

---
+++ src/components/PaymentForm.tsx
@@ -0,0 +1,11 @@
+// <== PAYMENT FORM COMPONENT ==>
+const PaymentForm = () => {
+  // RETURNING PAYMENT FORM COMPONENT
+  return (
+    // MAIN CONTAINER
+    <div>PaymentForm</div>
+  );
+};
+
+// <== EXPORTING PAYMENT FORM COMPONENT ==>
+export default PaymentForm;
