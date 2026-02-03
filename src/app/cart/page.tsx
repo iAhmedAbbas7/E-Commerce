0b34aaa53999a62233dec0eb7ee9d04488e82de4
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-03 18:30:04 +0500

    Refactor CartPage Component to Integrate Cart Functionality and Improve Step Rendering

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,20 +2,21 @@
 "use client";
 
 // <== IMPORTS ==>
-import Image from "next/image";
-import { useState } from "react";
-import { ShippingFormType } from "@/types/types";
-import PaymentForm from "@/components/PaymentForm";
-import { CART_PAGE_STEPS } from "@/utils/constants";
-import ShippingForm from "@/components/ShippingForm";
-import { useRouter, useSearchParams } from "next/navigation";
 import {
   ArrowRight,
   CreditCard,
+  ShoppingBag,
   ShoppingCart,
   Trash2,
   Truck,
 } from "lucide-react";
+import Image from "next/image";
+import { JSX, useState } from "react";
+import useCartStore from "@/store/CartStore";
+import { ShippingFormType } from "@/types/types";
+import PaymentForm from "@/components/PaymentForm";
+import ShippingForm from "@/components/ShippingForm";
+import { useRouter, useSearchParams } from "next/navigation";
 
 // <== CART PAGE STEPS ==>
 const STEPS = [
@@ -23,21 +24,24 @@ const STEPS = [
   {
     id: 1,
     title: "Shopping Cart",
+    icon: <ShoppingCart />,
   },
   // STEP 2
   {
     id: 2,
     title: "Shipping Address",
+    icon: <Truck />,
   },
   // STEP 3
   {
     id: 3,
     title: "Payment Method",
+    icon: <CreditCard />,
   },
 ];
 
 // <== CART PAGE COMPONENT ==>
-const CartPage = () => {
+const CartPage = (): JSX.Element => {
   // USING USE ROUTER HOOK
   const router = useRouter();
   // USING USE SEARCH PARAMS HOOK
@@ -48,6 +52,8 @@ const CartPage = () => {
   );
   // SETTING ACTIVE STEP
   const activeStep = parseInt(searchParams.get("step") || "1");
+  // GETTING CART AND REMOVE CART ITEM FUNCTION FROM USE CART STORE
+  const { cart, removeItem } = useCartStore();
   // RETURNING CART PAGE COMPONENT
   return (
     // MAIN CONTAINER
@@ -78,11 +84,19 @@ const CartPage = () => {
             </div>
             <div>
               {step.id === 1 && (
-                <ShoppingCart className="w-5 h-5 text-gray-500" />
+                <ShoppingCart
+                  className={`w-5 h-5 ${step.id === activeStep ? "text-gray-600" : "text-gray-200"}`}
+                />
               )}
-              {step.id === 2 && <Truck className="w-5 h-5 text-gray-500" />}
+              {step.id === 2 && (
+                <Truck
+                  className={`w-5 h-5 ${step.id === activeStep ? "text-gray-600" : "text-gray-200"}`}
+                />
+              )}
               {step.id === 3 && (
-                <CreditCard className="w-5 h-5 text-gray-500" />
+                <CreditCard
+                  className={`w-5 h-5 ${step.id === activeStep ? "text-gray-600" : "text-gray-200"}`}
+                />
               )}
             </div>
           </div>
@@ -93,14 +107,31 @@ const CartPage = () => {
         {/* STEPS */}
         <div className="w-full lg:w-7/12 shadow-lg border-2 border-gray-200 p-8 rounded-lg flex flex-col gap-8">
           {/* STEP TITLE */}
-          <h2 className="font-semibold text-lg">
-            {STEPS.find((step) => step.id === activeStep)?.title}
+          <h2 className="font-semibold text-lg flex items-center gap-2 text-gray-600">
+            <span className="mr-2">
+              {STEPS.find((step) => step.id === activeStep)?.icon}
+            </span>
+            <span>{STEPS.find((step) => step.id === activeStep)?.title}</span>
           </h2>
           {/* CONDITIONALLY RENDERING STEPS */}
+          {/* WHEN CART IS EMPTY */}
+          {cart.length === 0 && activeStep === 1 ? (
+            <div className="flex w-full h-full flex-col items-center justify-center gap-4">
+              <p className="text-gray-600 font-semibold">
+                Your cart is currently empty.
+              </p>
+              <ShoppingBag className="w-12 h-12 text-gray-400" />
+            </div>
+          ) : null}
+          {/* WHEN CART IS NOT EMPTY */}
           {activeStep === 1 ? (
-            CART_PAGE_STEPS.map((item) => (
+            cart.length !== 0 &&
+            cart.map((item) => (
               // SINGLE CART ITEM
-              <div key={item.id} className="flex items-center justify-between">
+              <div
+                key={item.id + item.selectedSize + item.selectedColor}
+                className="flex items-center justify-between"
+              >
                 {/* IMAGE & DETAILS */}
                 <div className="flex gap-8">
                   {/* PRODUCT IMAGE */}
@@ -134,6 +165,7 @@ const CartPage = () => {
                 {/* DELETE BUTTON */}
                 <button
                   title="Remove Item"
+                  onClick={() => removeItem(item)}
                   className="w-8 h-8 rounded-full bg-red-100 hover:bg-red-200 transition-all duration-300 text-red-400 flex items-center justify-center p-2 cursor-pointer"
                 >
                   <Trash2 />
@@ -153,18 +185,19 @@ const CartPage = () => {
         {/* CART DETAILS */}
         <div className="w-full lg:w-5/12 shadow-lg border-2 border-gray-200 p-8 rounded-lg flex flex-col gap-8 h-max">
           {/* TITLE */}
-          <h2 className="font-semibold">Cart Details</h2>
+          <h2 className="font-semibold flex items-center gap-2 text-lg text-gray-600">
+            <span>Cart Details</span>
+            <span>
+              <ShoppingBag className="w-4 h-4" />
+            </span>
+          </h2>
           {/* INFO SECTION */}
           <div className="flex flex-col gap-4">
             {/* SUBTOTAL */}
             <div className="flex items-center justify-between">
               <p className="text-sm font-semibold text-gray-600">Subtotal</p>
               <p className="text-sm font-semibold text-gray-600">
-                $
-                {CART_PAGE_STEPS.reduce(
-                  (acc, item) => acc + item.price,
-                  0,
-                ).toFixed(2)}
+                ${cart.reduce((acc, item) => acc + item.price, 0).toFixed(2)}
               </p>
             </div>
             {/* DISCOUNT */}
@@ -172,21 +205,43 @@ const CartPage = () => {
               <p className="text-sm font-semibold text-gray-600">
                 Discount (10%)
               </p>
-              <p className="text-sm font-semibold text-gray-600">$10.00</p>
+              <p className="text-sm font-semibold text-gray-600">
+                $
+                {cart
+                  .reduce(
+                    (acc, item) => acc + item.price * item.quantity * 0.1,
+                    0,
+                  )
+                  .toFixed(2)}
+              </p>
             </div>
             {/* SHIPPING FEE */}
             <div className="flex items-center justify-between">
               <p className="text-sm font-semibold text-gray-600">
                 Shipping Fee
               </p>
-              <p className="text-sm font-semibold text-gray-600">$5.00</p>
+              <p className="text-sm font-semibold text-gray-600">
+                {cart.length > 0 ? "$5.00" : "$0.00"}
+              </p>
             </div>
             {/* HORIZONTAL LINE */}
             <hr className="border-2 border-gray-200" />
             {/* TOTAL */}
             <div className="flex items-center justify-between">
               <p className="text-lg font-semibold text-gray-600">Total</p>
-              <p className="text-lg font-semibold text-gray-600">$15.00</p>
+              <p className="text-lg font-semibold text-gray-600">
+                $
+                {cart
+                  .reduce(
+                    (acc, item) =>
+                      acc +
+                      item.price * item.quantity -
+                      item.price * item.quantity * 0.1 +
+                      5,
+                    0,
+                  )
+                  .toFixed(2)}
+              </p>
             </div>
           </div>
           {/* BUTTON */}
