079651cb5c34c6e13e04e7b673f34d430a73828b
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-03 18:31:36 +0500

    Add PaymentForm Schema and CartStore Types for State Management

--- src/types/types.ts
+++ src/types/types.ts
@@ -73,3 +73,51 @@ export const ShippingFormSchemaType = z.object({
 
 // <== SHIPPING FORM TYPE ==>
 export type ShippingFormType = z.infer<typeof ShippingFormSchemaType>;
+
+// <== PAYMENT FORM SCHEMA TYPE ==>
+export const PaymentFormSchemaType = z.object({
+  // <== CARD HOLDER NAME FIELD ==>
+  cardHolderName: z.string().min(3, "Card Holder Name is required"),
+  // <== CARD NUMBER FIELD ==>
+  cardNumber: z
+    .string()
+    .min(16, "Card number must be at least 16 digits")
+    .max(16, "Card number must be at most 16 digits")
+    .regex(/^[0-9]+$/, "Card number must contain only digits"),
+  // <== EXPIRATION DATE FIELD ==>
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/?([0-9]{2})$/,
+      "Expiration date must be in MM/YY format",
+    ),
+  // <== CVV FIELD ==>
+  cvc: z
+    .string()
+    .min(3, "CVC must be at least 3 digits")
+    .max(4, "CVC must be at most 4 digits")
+    .regex(/^[0-9]+$/, "CVC must contain only digits"),
+});
+
+// <== PAYMENT FORM TYPE ==>
+export type PaymentFormType = z.infer<typeof PaymentFormSchemaType>;
+
+// <== CART STORE TYPE ==>
+export type CartStoreStateType = {
+  // <== CART STATE ==>
+  cart: CartItemListType;
+  // <== HAS HYDRATION FINISHED STATE ==>
+  hasHydrationFinished: boolean;
+  // <== SET HAS HYDRATION FINISHED ==>
+  setHasHydrationFinished: (value: boolean) => void;
+};
+
+// <== CART STORE ACTIONS TYPE ==>
+export type CartStoreActionsType = {
+  // <== ADD ITEM TO CART ==>
+  addItem: (item: CartItemType) => void;
+  // <== REMOVE ITEM FROM CART ==>
+  removeItem: (item: CartItemType) => void;
+  // <== CLEAR CART ==>
+  clearCart: () => void;
+};
