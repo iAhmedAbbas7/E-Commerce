82607510c358a585777464b3d49a3c5133480157
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-03 18:31:08 +0500

    Implement ShoppingCartIcon Component with Cart Quantity Display

--- src/components/ShoppingCartIcon.tsx
+++ src/components/ShoppingCartIcon.tsx
@@ -5,16 +5,23 @@
 import { JSX } from "react";
 import Link from "next/link";
 import { ShoppingCart } from "lucide-react";
+import useCartStore from "@/store/CartStore";
 
 // <== SHOPPING CART ICON COMPONENT ==>
 const ShoppingCartIcon = (): JSX.Element => {
+  // USING SHOPPING CART STORE
+  const { cart, hasHydrationFinished } = useCartStore();
+  // IF HYDRATION HAS NOT FINISHED, RETURN EMPTY FRAGMENT
+  if (!hasHydrationFinished) {
+    return <></>;
+  }
   // RETURN SHOPPING CART ICON COMPONENT
   return (
     // MAIN CONTAINER
     <Link href={"/cart"} className="relative">
       <ShoppingCart className="w-5 h-5 text-gray-600" />
       <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs">
-        0
+        {cart.reduce((total, item) => total + item.quantity, 0)}
       </span>
     </Link>
   );
